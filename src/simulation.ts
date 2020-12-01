aff5998d598bdfa18cd0d62292a36000678206fb
lparolari <luca.parolari23@gmail.com>
2020-12-01 22:01:44 +0100

    refactor(simulation): update

--- src/simulation.ts
+++ src/simulation.ts
@@ -1,26 +1,35 @@
 import _ from 'lodash'
-import { Point2D } from './point'
+import { makePoint, Point2D } from './point'
 import { rotate } from './rotate'
 
 export type Sun = 'sun'
 export type Planet = 'mercury'
 export type EntityId = Sun | Planet
 
+export type Speed = number
+
 export type Entity = {
   position: Point2D
-  rotationSpeed: number
+  rotationSpeed: Speed
 }
 
 export type System = {
   entities: Entity[]
 }
 
-export const fresh = (): System => ({
-  entities: [
-    { position: [0, 0], rotationSpeed: 0 },
-    { position: [0.4, 0], rotationSpeed: 4.1477 },
-  ],
+export const makeEntity = (rotationSpeed: Speed) => (position: Point2D): Entity => ({
+  rotationSpeed: rotationSpeed,
+  position: position,
 })
+export const makeSystem = (es: Entity[]): System => ({ entities: es })
+
+export const makeSun = makeEntity(0)
+export const makeMercury = makeEntity(4.1477)
+
+const freshSun = makeSun(makePoint(0, 0))
+const freshMercury = makeMercury(makePoint(0.4, 0))
+
+export const fresh = (): System => makeSystem([freshSun, freshMercury])
 
 const idx = (name: EntityId): number => {
   if (name === 'mercury') return 1
@@ -38,12 +47,7 @@ const forwardEntity = (tick: number) => (e: Entity): Entity => ({
 
 const forwardEntities = (tick: number) => (es: Entity[]): Entity[] => es.map(forwardEntity(tick))
 
-export const once = (system: System): System => {
-  const es: Entity[] = forwardEntities(0.0174533)(system.entities)
-  return {
-    entities: es,
-  }
-}
+export const once = (system: System): System => makeSystem(forwardEntities(0.0174533)(system.entities))
 
 export const simulate = function* (): Generator<System, System, System> {
   let system = fresh()
