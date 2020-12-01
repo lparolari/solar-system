3dad4bcb301bcf9294fa53cf58ef26b6dc9bf6a2
lparolari <luca.parolari23@gmail.com>
2020-12-01 21:55:16 +0100

    feat(solar-system): add simulation generator

--- src/__tests__/simulation.test.ts
+++ src/__tests__/simulation.test.ts
@@ -1,6 +1,10 @@
+import { getX, getY } from '../point'
+import { rotate } from '../rotate'
 import { System } from '../simulation'
 import { fresh } from '../simulation'
 import { position } from '../simulation'
+import { once } from '../simulation'
+import { simulate } from '../simulation'
 
 describe('fresh', () => {
   const system: System = fresh()
@@ -11,5 +15,51 @@ describe('fresh', () => {
 
   test('it puts mercury ad 0.4 au from sun', () => {
     expect(position('mercury')(system)).toEqual([0.4, 0])
+  })
+})
+
+describe('once', () => {
+  test('it rotates entity by one step from fresh', () => {
+    const system: System = fresh()
+    const rotatedSystem: System = {
+      entities: [
+        { position: [0, 0], rotationSpeed: 0 },
+        { position: rotate(0.0174533 * 4.1477)([0.4, 0]), rotationSpeed: 4.1477 },
+      ],
+    }
+
+    expect(once(system)).toEqual(rotatedSystem)
+  })
+
+  test('it rotates entity by one step from first step', () => {
+    const system: System = {
+      entities: [
+        { position: [0, 0], rotationSpeed: 0 },
+        { position: rotate(0.0174533 * 4.1477)([0.4, 0]), rotationSpeed: 4.1477 },
+      ],
+    }
+
+    const rotatedSystem: System = {
+      entities: [
+        { position: [0, 0], rotationSpeed: 0 },
+        { position: rotate(0.0174533 * 4.1477)(position('mercury')(system)), rotationSpeed: 4.1477 },
+      ],
+    }
+
+    expect(once(system)).toEqual(rotatedSystem)
+  })
+})
+
+describe('simulate', () => {
+  test('it returns first step equal to `once(fresh())`', () => {
+    const sExpected = once(fresh())
+
+    const iter = simulate()
+    iter.next() // fresh()
+    const curr = iter.next() // once(fresh())
+
+    // FIXME: We may use custom matcher
+    expect(getX(curr.value.entities[1].position)).toBeCloseTo(getX(sExpected.entities[1].position))
+    expect(getY(curr.value.entities[1].position)).toBeCloseTo(getY(sExpected.entities[1].position))
   })
 })
--- src/simulation.ts
+++ src/simulation.ts
@@ -1,26 +1,54 @@
 import _ from 'lodash'
 import { Point2D } from './point'
+import { rotate } from './rotate'
 
-export type Name = Sun | Planet
 export type Sun = 'sun'
 export type Planet = 'mercury'
+export type EntityId = Sun | Planet
 
 export type Entity = {
   position: Point2D
+  rotationSpeed: number
 }
+
 export type System = {
-  enities: Entity[]
+  entities: Entity[]
 }
 
 export const fresh = (): System => ({
-  enities: [{ position: [0, 0] }, { position: [0.4, 0] }],
+  entities: [
+    { position: [0, 0], rotationSpeed: 0 },
+    { position: [0.4, 0], rotationSpeed: 4.1477 },
+  ],
 })
 
-const idx = (name: Name): number => {
+const idx = (name: EntityId): number => {
   if (name === 'mercury') return 1
   return 0
 }
 
-export const position = (name: Name) => (system: System): Point2D => {
-  return system.enities[idx(name)].position
+export const position = (name: EntityId) => (system: System): Point2D => {
+  return system.entities[idx(name)].position
+}
+
+const forwardEntity = (tick: number) => (e: Entity): Entity => ({
+  ...e,
+  position: rotate(tick * e.rotationSpeed)(e.position),
+})
+
+const forwardEntities = (tick: number) => (es: Entity[]): Entity[] => es.map(forwardEntity(tick))
+
+export const once = (system: System): System => {
+  const es: Entity[] = forwardEntities(0.0174533)(system.entities)
+  return {
+    entities: es,
+  }
+}
+
+export const simulate = function* (): Generator<System, System, System> {
+  let system = fresh()
+  while (true) {
+    yield system
+    system = once(system)
+  }
 }
